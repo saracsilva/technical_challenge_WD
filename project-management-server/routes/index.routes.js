0849492e91d2d09248bb600c042788efd96bc216
saracsilva <saracarolinasilva@gmail.com>
2023-01-11 17:24:21 +0100

    create id route

--- project-management-server/routes/index.routes.js
+++ project-management-server/routes/index.routes.js
@@ -9,22 +9,34 @@ router.get("/", (req, res, next) => {
 
 router.get("/phones", async (req, res, next) => {
   fs.readFile(dataPath, "utf8", function (err, data) {
-    console.log(data);
+    //console.log(data, "test02");
     res.end(data);
   });
 
   // res.json(phones);
 });
-
-router.get("/phones/:id", async (req, res, next) => {
+router.get("/phones/:id", function (req, res) {
+  // First retrieve existing user list
+  fs.readFile(dataPath, "utf8", function (err, data) {
+    const phones = JSON.parse(data);
+    console.log(phones[0], "test");
+    const phone = phones[req.params.id];
+    console.log(phone, "sara");
+    res.end(JSON.stringify(phone));
+  });
+});
+/* router.get("/phones/:id", async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const phone = await Beer.findById(id);
+    const { phoneId } = req.params;
+    fs.readFile(dataPath, "utf8", function (err, data) {
+      console.log("test05", data.phoneId);
+      res.end(...data.phoneId);
+    });
 
-    res.json({ ...beer._doc });
+    // res.json({ ...beer._doc });
   } catch (error) {
     res.status(404).json({ message: "No beer with this id" });
   }
-});
+}); */
 
 module.exports = router;
